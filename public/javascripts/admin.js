37a8ce0f3085663891d358ab18eea832ae375063
transformGit <2630243397@qq.com>
2016-09-22 17:46:13 +0800

    海报预览

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -23,4 +23,13 @@ $(function(){
 			})
 		}
 	})
+	$('#uploadPost').change(function(){
+			if(this.files && this.files[0]){
+			var reader = new FileReader();
+			reader.onload = function(e){
+					$('#imgPreview').attr('src', e.target.result)
+			}
+			reader.readAsDataURL(this.files[0])
+		}
+	})
 })
